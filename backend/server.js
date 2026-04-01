5d50bd5d904605a0b4d54edb1f1e8126a5b1e746
Hadjoudj Abdelghafour <ahmedhadjoudj303@gmail.com>
2026-04-01 21:08:09 +0100

    link changing

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://gueddoudacenter.vercel.app"],
+    origin: ["http://localhost:5173", "https://abdelghafour.vercel.app"],
     credentials: true,
   })
 );
